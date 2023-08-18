71e02a0e44920eb955573461185a008d43dfb303
nikolai <northfacebodybags@yahoo.com>
2023-08-18 16:30:53 +0400

    feat: UILD-24: Add internationalization (i18n) support

--- src/Wrapper.js
+++ src/Wrapper.js
@@ -1,14 +1,42 @@
 import '@folio-eis/marva-next';
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
 
+const OKAPI_CONFIG = 'okapi_config';
 
-const Wrapper = (props) => {
-  // TODO: manage & purge localStorage here
+const Wrapper = ({ stripes }) => {
+  useEffect(() => {
+    // url, tenant, token, locale, tz
+    const {
+      locale,
+      timezone,
+      okapi: {
+        url,
+        tenant,
+        token
+      },
+    } = stripes || {};
+
+    const config = {
+      locale,
+      timezone,
+      url,
+      tenant,
+      token,
+    };
+
+    localStorage.setItem(OKAPI_CONFIG, JSON.stringify(config));
+  }, [stripes]);
 
   return (
     <div id="editor-root" style={{ margin: '1em' }}>
-      <marva-next route-prefix="/marva" okapi={JSON.stringify(props.stripes.okapi)} />
+      <marva-next route-prefix="/marva" />
     </div>
   );
+};
+
+Wrapper.propTypes = {
+  stripes: PropTypes.object.isRequired,
 };
 
 export default Wrapper;
